b6daabd670f6c9a1029d67e925115ee2527c10f5
Tomáš Križák <tomaskrizak@gmail.com>
2024-07-04 21:57:31 +0200

    add delete from cart function and save cart to localStorage for every item added/deleted

--- data/cart.js
+++ data/cart.js
@@ -1,7 +1,21 @@
-export let cart = [
-  { productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', quantity: 2 },
-  { productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d', quantity: 1 },
-];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+  cart = [
+    {
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+    },
+    {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1,
+    },
+  ];
+}
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 // add item to cart based on product id, works with same item
 
@@ -22,7 +36,11 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+
+  saveToStorage();
 }
+
+// create new cart array that will save all items with different product id
 
 export function removeFromCart(productId) {
   const newCart = [];
@@ -33,4 +51,8 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToStorage();
 }
+
+console.log(cart);
