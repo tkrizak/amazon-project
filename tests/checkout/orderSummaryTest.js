76137680f8dbbc3fd9de088cd4c0def393e419c3
Tomáš Križák <tomaskrizak@gmail.com>
2024-07-18 17:17:55 +0200

    convert loadProducts from XHR to fetch

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,6 +1,6 @@
 import { loadFromStorage, cart } from '../../data/cart.js';
 import { renderOrderSummary } from '../../scripts/checkout/orderSummary.js';
-import { loadProducts } from '../../data/products.js';
+import { loadProducts, loadProductsFetch } from '../../data/products.js';
 
 describe('test suite: renderOrderSummary', () => {
   // setup for tests
@@ -9,7 +9,7 @@ describe('test suite: renderOrderSummary', () => {
   const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
 
   beforeAll((done) => {
-    loadProducts(() => {
+    loadProductsFetch().then(() => {
       done();
     });
   });
